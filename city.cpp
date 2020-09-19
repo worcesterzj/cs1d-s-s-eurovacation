9e936fa528de76cefa94d0854a19a9edd66d6afa
Stuka55 <63323662+Stuka55@users.noreply.github.com>
2020-09-18 19:03:02 -0700

    Update city.cpp

--- city.cpp
+++ city.cpp
@@ -1,11 +1,34 @@
 #include "city.h"
 
-city::city(const city& other) : name{ other.name }, parentCountry{ other.parentCountry } {
+city::city(const city& other) : name{ other.name }, parentCountry{ other.parentCountry } 
+{
 /*    if(!other.foodItems.empty())                                     // uncomment when food exists :)
         for(int i = 0; i < other.foodItems.size(); i++)
             this->foodItems.push_back(other.foodItems[i]);             */
 }
 
-QString city::getName() const {
+QString city::getName() const 
+{
     return name;
+}
+
+Qstring city::getname() const
+{
+        return parentCountry;
+}
+
+void setName(Qstring name)
+{
+        this->name = name;
+}
+
+void setparent(Qstring parentCountry)
+{
+        this->parentCountry = parentCountry;
+}
+
+void addFoodItem(food foodItem)
+{
+        foodItems.pushBack(foodItem);
+        // or whatever the funtion call would be
 }
